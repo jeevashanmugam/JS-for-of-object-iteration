0abd4e283ba670250529b4ad828acac9f56597ed
jeevashanmugam <jeevasj2000@gmail.com>
2023-06-20 20:20:11 +0530

    JS-for-of-object-iteration

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let arr = ["Shanumgam", "Jaya", "jeeva", "shalini", 18, 24, 2, 3, 4]
+
+for (let newValue of arr) {
+
+    //document.write(newValue + " ") // its does not work 
+    //document.write(arr[newValue] + " ") // its does not work 
+    document.write(newValue + " ")
+
+}
+
+
+
+
